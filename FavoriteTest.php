557c098f5c0531ff2faad36a1070fc7c4f67d49c
dev <enfn2001@gmail.com>
2015-05-14 02:42:01 +0900

    Favorite 사이트 클래스 - 사이트 항목 만들기

---
+++ FavoriteTest.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: dev
+ * Date: 15. 5. 14.
+ * Time: 오전 2:07
+ */
+
+class FavoriteTest extends PHPUnit_Framework_TestCase {
+
+}
--- favorite.php
+++ favorite.php
@@ -9,13 +9,24 @@
 class Favorite
 {
     public function getFullLink(){
-        $htmlStr = "
-            <h2> Favorite Site </h2>
-            <ul>
-                <li><a href='http://ropipi.com/' target='_blank'>비밀 일기장</a></li>
-                <li><a href='http://ropipi.com/' target='_blank'>책독서 후기</a></li>
-            </ul>";
+        $site_url = array(
+            "http://ropipi.com",
+            "http://boooki.com/ko/"
+        );
+        $site_title = array(
+            "비밀 일기장",
+            "독후감"
+        );
+
+        //<li><a href='http://ropipi.com/' target='_blank'>비밀 일기장</a></li>
+        $htmlStr = "<h2> Favorite Site </h2><ul>";
+
+        for($i = 0 ; $i < count($site_url); $i++)
+            $htmlStr = $htmlStr . "<li><a href='".$site_url[$i]."' target='_blank'>".$site_title[$i]."</a></li>";
+
+        $htmlStr = $htmlStr ."</ul>";
 
         return $htmlStr;
+
     }
 }
