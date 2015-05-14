0c3fe6c14517333ff421157f413120c2385fbf33
dev <enfn2001@gmail.com>
2015-05-15 00:56:28 +0900

    클래스화 구조화 진행중

--- Make.php
+++ Make.php
@@ -8,16 +8,21 @@
 
 class Make {
     public function getFullLink(){
-        $site_url = array(
-            "http://ucloud.duru.pe.kr/fcmd",
-            "http://codingeverybody.com"
-        );
+
+        $title = "I made app";
+
         $site_title = array(
             "facebook app - opentutorials facebook community",
             "facebook app - Searchable, quickFacebook"
         );
 
-        $htmlStr = "<h2> I made app </h2><ul>";
+        $site_url = array(
+            "http://ucloud.duru.pe.kr/fcmd",
+            "http://codingeverybody.com"
+        );
+
+
+        $htmlStr = "<h2>". $title ."</h2><ul>";
 
         for($i = 0 ; $i < count($site_url); $i++)
             $htmlStr = $htmlStr . "<li><a href='".$site_url[$i]."' target='_blank'>".$site_title[$i]."</a></li>";
---
+++ Thoughts.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: dev
+ * Date: 15. 5. 14.
+ * Time: 오후 11:18
+ */
+
+class Thoughts {
+    public function getFullHtml()
+    {
+        $title = "Thoughts";
+
+        $site_title = array(
+            "get thinks"
+        );
+
+        $site_url = array(
+            "http://www.duru.pe.kr/get.thinks"
+        );
+
+    }
+
+}
