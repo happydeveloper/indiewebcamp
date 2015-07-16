ef361bcc208a9b7687f21dd33908fa58e54e58e1
dev <enfn2001@gmail.com>
2015-07-16 15:29:51 +0900

    Just Simple

--- ResourceManager.php
+++ ResourceManager.php
@@ -8,20 +8,36 @@
 
 class ResourceManager {
 
-    static $txtMessage1 = "다양한 곳에 남겨 있는 나의 생각들, 나의 글들, 나의 사진, 나의 콘텐츠를 관리하는 출입문을 만들고 있다.
+    static $drafting1 = "
+    다양한 곳에 남겨 있는 나의 생각들 글들, 사진, 콘텐츠를 관리하는 체계를 만들고 있다.
+    I am making system
     지금은 고마운 웹 서비스들의 회사들이 나의 글들을, 나의 생각들, 나의 사진들을 잘 관리해주고 있다.
+
     하지만 언제까지 그 서비스들이 나를 위해서만 있는 것은 아니라는 생각이 들었다.
+
+
     사용자들은 결국 웹서비스의 통제를 받아야 하고 서비스 회사가 만든 규율을 지켜야 한다.
+
     그것이 나쁜 것은 아니라고 생각하고 대부분의 상식의 범위에서 일들이 처리된다.
+
     하지만 가끔은 사용자의 이해와 상충될 때가 있다.
+
     그리고 그 때는 사용자는 작아진다.
+
     다행히 나는 데이타와 정보를 관리하는 기술을 알고 있다.
+
     그런 경험과 지식은 자유라고 가진다.
+
     내가 사이트를 만든 이유는 데이타의 통제권을 나에게 가져오고 다양한 웹서비스로 부터 자유로워지길 원한다.
+
     복잡하지 않고 단순한 내 법칙을 만들어 가고 싶다.";
 
-    
-    static $txtMessage2 = "I am making doors that manage proof that my artcles, memo, taught, think, contents.";
 
+    static $drafting2 = "I am making doors that manage proof that my artcles, memo, taught, think, contents.";
+
+
+    static $txtMessage1 = "Just Simple;; ";
+
+    static $txtMessage2 = "단지 간단하게;;";
 
 }
