c0df5458182fc249e75b7570cdd5d6d71223655d
dev <enfn2001@gmail.com>
2015-11-28 15:41:03 +0900

    2015-11-26 올해 첫눈이 왔다. 날씨는 정말 추웠다. 최저 기온이 영하 -3도 였다. 칼바람이 불었다. 첫눈은 그런데 동심을 불러준다.~

--- upload.sh
+++ upload.sh
@@ -1,2 +1,2 @@
 
-scp -rp playGround classCore classRepository classThink classView html *.html *.php *.js happydeveloper@www.duru.pe.kr:/home/hosting_users/happydeveloper/www
+scp -rp css playGround classCore classRepository classThink classView html *.html *.php *.js happydeveloper@www.duru.pe.kr:/home/hosting_users/happydeveloper/www
